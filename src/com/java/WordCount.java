fcff65fff743efe7bffb701bb908bc16dac7f29f
Jaden-1997 <LDJsoCool@163.com>
2020-04-30 19:24:38 +0800

    16:58 begin

---
+++ src/com/java/WordCount.java
@@ -0,0 +1,7 @@
+package com.java;
+
+public class WordCount {
+    public static void main(String[] args) {
+        System.out.println("哈哈哈哈");
+    }
+}
--- src/com/test/WordCount.java
+++
@@ -1,9 +0,0 @@
-package com.test;
-
-public class WordCount {
-
-    public static void main(String[] args) {
-        System.out.println("gotostudy");
-        System.out.println("how to commit");
-    }
-}
