e7ea4fd955043a6af5281f97c311280144af3c3f
Jaden-1997 <LDJsoCool@163.com>
2020-05-01 17:32:14 +0800

    16:58 begin

--- src/com/java/WordCount.java
+++ src/com/java/WordCount.java
@@ -1,7 +1,8 @@
 package com.java;
 
-public class WordCount {
-    public static void main(String[] args) {
-        System.out.println("哈哈哈哈");
-    }
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordCount extends Mapper {
+
+
 }
